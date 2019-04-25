b1fec073534ed2c34812d229f9bc4519e416a2b2
rkaldawy <remykaldawy@gmail.com>
2019-04-25 18:25:49 -0400

    adding command line option for framerate

--- gifpaper.c
+++ gifpaper.c
@@ -2,16 +2,42 @@
 
 int main(int argc, char **argv)
 {
-  if(argc < 2){
-    printf("Usage: gifpaper gif");
-    return -1;
+
+  long framerate = 6;
+  int opt;
+
+  char *endptr;
+
+  while ((opt = getopt(argc, argv, "f:")) != -1){
+    switch(opt){
+      case 'f': 
+        framerate = strtol(optarg, &endptr, 10);
+        if(*optarg == '\0' || *endptr != '\0'){
+          printf("Error: framerate argument not an integer.\n");
+          return -1;
+        }
+        if(framerate <= 0 || framerate > 60){
+          printf("Error: Framerate must be between 1Hz and 60Hz\n");
+          return -1;
+        }
+        break;
+      default:
+        printf("Usage: gifpaper [-f] wallpaper.gif\n");
+        return -1;
+    }
   }
-  char *gifpath = argv[1];
+
+  if(optind >= argc){
+    printf("Usage: gifpaper [-f] wallpaper.gif\n");
+    return-1;
+  }
+  char *gifpath = argv[optind];
 
   init_x_and_imlib();
 
   int ret = break_gif_into_images(gifpath);
   if(ret < 0){
+    printf("Error: file was not readable.\n");
     return -1;
   }
 
@@ -22,7 +48,7 @@ int main(int argc, char **argv)
   
   struct timespec w;
   w.tv_sec = 0;
-  w.tv_nsec = 166666666 / 2;
+  w.tv_nsec = 999999999 / framerate; //1 second divided by frame rate
   
   while(1){
     set_background(head->pmap);
